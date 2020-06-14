07f33b0ba053a2ddddaa964c892b8e1d887ba1a8
Vladislav Paskar <vladislav.paskar@etikett.de>
2020-06-14 21:39:07 +0200

    add .gitignore

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/static/css/style.css">
+
+    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <title><?= getenv(APP_NAME); ?></title>
   </head>
   <body>
